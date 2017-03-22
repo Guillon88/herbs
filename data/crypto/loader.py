f60602fe7c82c04f9be2b3cc7f164b1de725c14c
Kislov Dmitry <kislov@easydan.com>
2017-03-23 09:13:13 +1000

    Preparing for bulk importing:

--- data/crypto/loader.py
+++ data/crypto/loader.py
@@ -36,7 +36,14 @@ frame = frame.applymap(str.lower)
 
 
 
-
+def split_species(sp):
+    _sp = sp.split()
+    if len(_sp) == 1:
+        raise IndexError
+    if len(_sp) == 2:
+        return _sp[0], _sp[1], ''
+    elif len(_sp) >= 3:
+        return _sp[0], _sp[1], ' '.join(_sp[3:])
 
 def suggest_species(sp, syns=syns, vbgi=vbgitaxa, main=frame):
     _syns = syns.copy()
@@ -54,36 +61,39 @@ def suggest_species(sp, syns=syns, vbgi=vbgitaxa, main=frame):
 
     if _sp[1] == 'cf.' or _sp[1] == 'cf':
         return (_sp[0], _sp[2], ' '.join(_sp[3:]) if (len(_sp) >= 4) else (_sp[0], _sp[2], '')
+
+    if _sp[1] == 'sp' or _sp[1] == 'sp.':
+        return (_sp[0], 'sp.', '')
     # --------------------------------------------------
 
     authorship = ' '.join(_sp[2:]) if len(_sp) >= 3 else: ''
     if len(_sp) >= 2:
-        if any((main['Genus'] == _sp[0]) & (main['Species'] == _sp[1])):
+        if any((main['Genus'] == _sp[0]) & (main['Species'] == _sp[1])) & (main['Authorship'] == authorship):
             return  (_sp[0], _sp[1], authorship)
 
     ix = _syns['current'].map(lambda x: x.startswith(sp))
     filtered = syns[ix]
-    if len(filtered) >= 1:
+    if len(filtered) == 1:
         if 'the same' in filtered.iloc[0]['approved']:
-            return filtered.iloc[0]['current']
+            return split_species(filtered.iloc[0]['current'])
         else:
-            return filtered.iloc[0]['approved']
-    else:
-        pass
+            return split_species(filtered.iloc[0]['approved'])
+    elif len(filtered) > 1:
+        print('%s: %s items found' % (sp, len(filtered)))
     nix = _syns['approved'].map(lambda x: x.startswith(sp))
     nfiltered = syns[nix]
-    if len(nfiltered) >= 1:
-        return nfiltered.iloc[0]['approved']
-    else:
-        pass
+    if len(nfiltered) == 1:
+        return split_species(nfiltered.iloc[0]['approved'])
+    elif len(nfiltered) > 1:
+        print('%s: %s items found' % (sp, len(nfiltered)))
+
     vbgix = _vbgi['current'].map(lambda x: x.startswith(sp))
     vfiltered = vbgi[vbgix]
-    if len(vfiltered) >= 1:return vfiltered.iloc[0]['current']
-
-
-    return None
-
-
+    if len(vfiltered) == 1:
+        return split_species(vfiltered.iloc[0]['current'])
+    elif len(vfiltered) > 1:
+        print('%s: %s items found' % (sp, len(nfiltered)))
+    return split_species(sp)
 
 def extract_species(x):
     ns = x.replace('together with', '').strip()
@@ -91,24 +101,12 @@ def extract_species(x):
     return list(filter(lambda x: len(x)>0, alist))
 
 
-
-#print(suggest_species('nardia les'))
-
-notfound = []
 for ind, row in data.iterrows():
     if not pd.isnull(row['comment']):
        comm = extract_species(row['comment'])
        if comm:
            for item in comm:
-               what = ' '.join(item.strip().lower().split()[:2])
-               rc = suggest_species(what)
-               if rc is None:
-                    print('(Comment)', ind + 2, item)
-                    notfound.append(what)
-    if not pd.isnull(row['species']):
-        res = suggest_species(row['species'].strip().lower())
-        if res is None:
-            print('(Species)', ind+2, row['species'])
-            notfound.append(row['species'].lower().strip())
-
-print("\nUnique species, total:", len(list(pd.np.unique(notfound))))
+               print(suggest_species(item))
+   if not pd.isnull(row['species']):
+       res = suggest_species(row['species'].strip().lower())
+       print(res)
