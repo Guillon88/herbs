47c10cfe909e30259dfe13230a5186951a4e6fd8
Kislov Dmitry <kislov@easydan.com>
2018-07-03 08:46:07 +1000

    hieroglyphs support... under construction

--- herbs/hlabel.py
+++ herbs/hlabel.py
@@ -200,6 +200,37 @@ class PDF_MIXIN(object):
     def create_file(self, fname):
         self.pdf.output(fname, dest='F')
 
+    def is_hieroglyph(self, s):
+        ranges = [
+            {"from": ord(u"\u3300"), "to": ord(u"\u33ff")},
+            # compatibility ideographs
+            {"from": ord(u"\ufe30"), "to": ord(u"\ufe4f")},
+            # compatibility ideographs
+            {"from": ord(u"\uf900"), "to": ord(u"\ufaff")},
+            # compatibility ideographs
+            {"from": ord(u"\U0002F800"), "to": ord(u"\U0002fa1f")},
+            # compatibility ideographs
+            {"from": ord(u"\u30a0"), "to": ord(u"\u30ff")},  # Japanese Kana
+            {"from": ord(u"\u2e80"), "to": ord(u"\u2eff")},
+            # cjk radicals supplement
+            {"from": ord(u"\u4e00"), "to": ord(u"\u9fff")},
+            {"from": ord(u"\u3400"), "to": ord(u"\u4dbf")},
+            {"from": ord(u"\U00020000"), "to": ord(u"\U0002a6df")},
+            {"from": ord(u"\U0002a700"), "to": ord(u"\U0002b73f")},
+            {"from": ord(u"\U0002b740"), "to": ord(u"\U0002b81f")},
+            {"from": ord(u"\U0002b820"), "to": ord(u"\U0002ceaf")}
+            # included as of Unicode 8.0
+        ]
+
+        def is_cjk(char):
+            return any(
+                [range["from"] <= ord(char) <= range["to"] for range in ranges])
+
+        return all(map(is_cjk, s.strip()))
+
+
+    def set_current_font(self, name, size, sample=''):
+        pass
 
     def smarty_print(self, txt, y,  left_position=0,
                      first_indent=10, right_position=10,
